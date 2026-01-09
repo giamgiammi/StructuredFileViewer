236b526562f9094e317cbd82dc7fbba7213e43ae
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-01-09 23:17:49 +0100

    Add CSV settings UI with new properties and initialize `CsvSettingsController`.

--- src/main/java/com/github/giamgiammi/StructuredFileViewer/ui/csv/CsvSettingsController.java
+++ src/main/java/com/github/giamgiammi/StructuredFileViewer/ui/csv/CsvSettingsController.java
@@ -1,8 +1,19 @@
 package com.github.giamgiammi.StructuredFileViewer.ui.csv;
 
 import com.github.giamgiammi.StructuredFileViewer.core.csv.CsvDataModel;
+import com.github.giamgiammi.StructuredFileViewer.core.csv.CsvDataModelFactory;
+import javafx.fxml.Initializable;
 
-public class CsvSettingsController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CsvSettingsController implements Initializable {
+    private final CsvDataModelFactory factory = new CsvDataModelFactory();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //
+    }
 
     public CsvDataModel getModel() {
         return null;//todo
