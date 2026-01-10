9ec23778ac64e791a1f373269d35093b84b3708d
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-01-10 14:48:39 +0100

    Add `build_windows.cmd` script for packaging with `jpackage` and runtime image creation

---
+++ build_windows.cmd
@@ -0,0 +1,6 @@
+echo Start image build
+call mvnw clean javafx:jlink
+
+echo Start packaging
+
+call jpackage -m com.github.giamgiammi.StructuredFileViewer/com.github.giamgiammi.StructuredFileViewer.App --runtime-image target/image --name "Structured File Viewer" --dest target/ --type app-image --icon logo.ico
