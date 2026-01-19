e306d6acd09212455e7f90d5b8002bccd617b51b
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-01-19 19:25:32 +0100

    Add macOS build script and application icon

---
+++ build_mac.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DIR="$(dirname "$0")"
+cd "$DIR" || exit 1
+
+set -e
+
+echo "Start image build"
+./mvnw clean javafx:jlink
+
+echo "Start packaging"
+jpackage -m com.github.giamgiammi.StructuredFileViewer/com.github.giamgiammi.StructuredFileViewer.App \
+             --runtime-image target/image \
+             --name 'Structured File Viewer' \
+             --dest target/ \
+             --type dmg \
+             --icon logo.icns \
+             --java-options "--enable-native-access=javafx.graphics"
+
+
+#echo "Copying license files"
+#cp -r target/legal "target/Structured File Viewer/"
+#cp target/classes/LICENSE.txt "target/Structured File Viewer/legal/LICENSE.txt"
