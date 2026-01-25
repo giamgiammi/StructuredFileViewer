558aa915b81d2b4836b33241e8c735d73ed11d86
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-01-25 11:55:07 +0100

    deleted unused CannotAcquireLockException class

--- src/main/java/com/github/giamgiammi/StructuredFileViewer/utils/CannotAcquireLockException.java
+++
@@ -1,7 +0,0 @@
-package com.github.giamgiammi.StructuredFileViewer.utils;
-
-public class CannotAcquireLockException extends RuntimeException {
-    public CannotAcquireLockException(String message) {
-        super(message);
-    }
-}
