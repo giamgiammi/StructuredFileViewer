7ac719fa6e22c2fd53a24d519966f2652648a3bb
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-01-27 18:27:42 +0100

    Update `TableFilterTest` to enhance regex-based filtering logic coverage

--- src/test/java/com/github/giamgiammi/StructuredFileViewer/filters/TableFilterTest.java
+++ src/test/java/com/github/giamgiammi/StructuredFileViewer/filters/TableFilterTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TableFilterTest {
     @Test
     void expr1Test() {
-        val query = "col1 = 'a' AND ($2 > 'h' OR $0 > '3')";
+        val query = "(col1 = 'a' AND ($2 > 'h' OR $0 > '3')) OR \"col2\" regex '[0-9]+'";
 
         val data = new SimpleTableData(List.of("col1", "col2"), List.of(
                 new String[] {"a", "aaaa"},
@@ -20,13 +20,15 @@ class TableFilterTest {
                 new String[] {"a", "dddd"},
                 new String[] {"a", "eeeee"},
                 new String[] {"b", "fffff"},
-                new String[] {"b", "ggggg"}
+                new String[] {"b", "ggggg"},
+                new String[] {"b", "12345"}
         ));
 
         val expected = new SimpleTableData(data.getColumnNames(), List.of(
                 new String[] {"a", "iiiii"},
                 new String[] {"a", "dddd"},
-                new String[] {"a", "eeeee"}
+                new String[] {"a", "eeeee"},
+                new String[] {"b", "12345"}
         ));
 
         val filter = TableFilter.parse(query, data.getColumnNames());
