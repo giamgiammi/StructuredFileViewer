a96ce573fceb77f7e1edefc57a6bf5e4e2e5689c
Gianmarco Carraretto <giamgiammi@gmail.com>
2026-02-01 12:37:41 +0100

    Add AppImage packaging support and improve jlink configuration
    - Introduce AppImage generation, including downloading `appimagetool` and setting up AppDir structure.
    - Add configuration for custom compression in `jlink`, defaulting to property-driven values.
    - Update `.gitignore` to exclude `cache/` directory.

--- build_linux.sh
+++ build_linux.sh
@@ -9,7 +9,8 @@ echo "Start project build"
 ./mvnw clean compile
 
 echo "Start image build"
-./mvnw javafx:jlink -Pforce-modular
+# Note: AppImage compression seems to work better than jlink one
+./mvnw -Djlink.compress=0 javafx:jlink -Pforce-modular
 
 echo "Start packaging"
 jpackage -m com.github.giamgiammi.StructuredFileViewer/com.github.giamgiammi.StructuredFileViewer.App \
@@ -24,3 +25,24 @@ jpackage -m com.github.giamgiammi.StructuredFileViewer/com.github.giamgiammi.Str
 echo "Copying license files"
 cp -r target/legal "target/Structured File Viewer/"
 cp target/classes/LICENSE.txt "target/Structured File Viewer/legal/LICENSE.txt"
+
+if ! [ -f "cache/appimagetool-x86_64.AppImage" ]; then
+  echo "Downloading appimagetool"
+  wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O "cache/appimagetool-x86_64.AppImage"
+  chmod +x "cache/appimagetool-x86_64.AppImage"
+fi
+
+echo "Preparing AppImage package"
+mv "target/Structured File Viewer" "target/Structured File Viewer.AppDir"
+(cd "target/Structured File Viewer.AppDir" && ln -s "bin/Structured File Viewer" AppRun)
+cp src/main/resources/com/github/giamgiammi/StructuredFileViewer/logo.png "target/Structured File Viewer.AppDir/logo.png"
+echo "[Desktop Entry]" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Name=Structured File Viewer" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Name[it]=Visualizzatore di file strutturato" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Exec=AppRun" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Icon=logo" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Type=Application" >> "target/Structured File Viewer.AppDir/app.desktop"
+echo "Categories=Utility;" >> "target/Structured File Viewer.AppDir/app.desktop"
+
+echo "Packaging AppImage"
+"cache/appimagetool-x86_64.AppImage" "target/Structured File Viewer.AppDir" "target/Structured File Viewer"
